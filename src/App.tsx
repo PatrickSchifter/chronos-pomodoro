5ea97bb34ff6ecfc0151ead25a9c8c33ec14dc8d
Patrick Schifter <schiftercorp@outlook.com>
2025-06-12 10:50:10 -0300

    feat: add types to heading props and lucide-react lib

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Paragrafo</Heading>
+      <Heading>
+        Paragrafo
+        <button>
+          <TimerIcon />
+          Timer
+        </button>
+      </Heading>
       <h1>This is APP.tsx</h1>
     </>
   );
--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,33 +1,9 @@
-import type {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-} from 'react';
 import styles from './index.module.css';
 
-export function Heading(props: {
-  children:
-    | string
-    | number
-    | bigint
-    | boolean
-    | ReactElement<unknown, string | JSXElementConstructor<unknown>>
-    | Iterable<ReactNode>
-    | ReactPortal
-    | Promise<
-        | string
-        | number
-        | bigint
-        | boolean
-        | ReactPortal
-        | ReactElement<unknown, string | JSXElementConstructor<unknown>>
-        | Iterable<ReactNode>
-        | null
-        | undefined
-      >
-    | null
-    | undefined;
-}) {
-  return <h1 className={styles.title}>{props.children}</h1>;
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
