c6cc84feb5f931bdeda86d5ab3a76ecc1ac0ab77
Patrick Schifter <schiftercorp@outlook.com>
2025-06-15 16:00:13 -0300

    feat: add default layout to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import { Logo } from './components/Logo';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
-      <Logo />
-    </div>
+    <>
+      <div className='container-fluid'>
+        <div className='container'>
+          <div className='content'>
+            <section>Lorem ipsum</section>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
