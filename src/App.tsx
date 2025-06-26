04327725b9a53be262e69993a16a24cf6a81fc08
Patrick Schifter <schiftercorp@outlook.com>
2025-06-26 14:08:13 -0300

    feat: created DefaultInput component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -17,6 +18,25 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
