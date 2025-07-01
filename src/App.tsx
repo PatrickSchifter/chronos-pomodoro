4eeea89e99e6c49be0c845162e4806ea832ab636
Patrick Schifter <schiftercorp@outlook.com>
2025-06-30 22:07:11 -0300

    feat: add all props to DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export function App() {
       <Container>
         <form className='form'>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput id='meuInput' type='text' labelText='task' />
           </div>
           <div className='formRow'>
             <p>Lorem ipsum dolor sit amet.</p>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,12 +1,18 @@
 type DefaultInputProps = {
   id: string;
+  labelText: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
-      <label htmlFor=''>task</label>
-      <input id={id} type={type} />
+      <label htmlFor=''>{labelText}</label>
+      <input id={id} type={type} {...rest} />
     </>
   );
 }
